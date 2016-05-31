224028fccf72f504e14f7af60e17dec4fd57aa33
Beniamino Negrini <beniamino.negrini@gmail.com>
2016-05-31 19:27:44 +0200

    fatto il main

---
+++ src/com/RSA/Main.java
@@ -0,0 +1,45 @@
+/**
+ * 
+ */
+package com.RSA;
+import com.RSA.view.VHome;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+/**
+ * @author Eugenio
+ *
+ */
+public class Main extends Application {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		launch(args);
+	}
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		Parent root = FXMLLoader.load(getClass().getResource("view/JavaFX/xml/home.fxml"));
+
+		primaryStage.setTitle("Rsa");
+		primaryStage.setFullScreen(true);
+		primaryStage.setScene(new Scene(root));
+
+		//primaryStage.getIcons().add(new Image("file:../../../../Image/tankicon.png"));
+
+        /*
+        primaryStage.getIcons().add(
+                new Image(
+                        TankWarMain.class.getResourceAsStream( "../../../../Image/tankicon.png" )));
+        */
+
+
+		primaryStage.show();
+
+	}
+}
