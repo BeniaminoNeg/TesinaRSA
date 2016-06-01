dda97a7d2ae3dc9953c522a7eb95140a3757b386
Emanuele Fianco <emanuele.fianco@gmail.com>
2016-06-01 13:21:02 +0200

    Aggiunta creazione chiave privata

---
+++ src/com/RSA/view/JavaFX/Util/Creatori/CreatoreScrollPane.java
@@ -0,0 +1,32 @@
+package com.RSA.view.JavaFX.Util.Creatori;
+
+import com.RSA.view.JavaFX.Util.ConvertitorePx;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.layout.VBox;
+/**
+ * Created by emanuele on 01/06/16.
+ */
+public class CreatoreScrollPane {
+
+    public static ScrollPane creaScrollPane(VBox content, double minWidthPercentuale, double paddingAltoPercentuale, double paddingBassoPercentuale, boolean visibile) {
+        ScrollPane scrollPane = new ScrollPane();
+        layoutizeScrollPane(scrollPane,minWidthPercentuale,paddingAltoPercentuale,paddingBassoPercentuale,visibile);
+        riempiScrollPane(scrollPane,content);
+        return scrollPane;
+    }
+
+    public static void layoutizeScrollPane(ScrollPane scrollPane, double minWidthPercentuale, double paddingAltoPercentuale, double paddingBassoPercentuale, boolean visibile) {
+        double paddingAlto = ConvertitorePx.percentualeToPxAltezza(paddingAltoPercentuale);
+        double paddingBasso = ConvertitorePx.percentualeToPxAltezza(paddingBassoPercentuale);
+        scrollPane.setStyle("-fx-padding: "+paddingAlto+"px 0px "+paddingBasso+"px 0px;");
+        double minWidth = ConvertitorePx.percentualeToPxLarghezza(minWidthPercentuale);
+        scrollPane.setMaxWidth(minWidth);
+        scrollPane.setMinWidth(minWidth);
+        scrollPane.setHbarPolicy(ScrollPane.ScrollBarPolicy.ALWAYS);
+        scrollPane.setVisible(visibile);
+    }
+
+    public static void riempiScrollPane(ScrollPane scrollPane, VBox content) {
+        scrollPane.setContent(content);
+    }
+}
--- src/com/RSA/view/JavaFX/View/VHome.java
+++ src/com/RSA/view/JavaFX/View/VHome.java
@@ -1,20 +1,20 @@
 package com.RSA.view.JavaFX.View;
 
 import com.RSA.model.algoritmoRSA.Client;
+import com.RSA.model.algoritmoRSA.GeneratoreChiavi;
 import com.RSA.view.JavaFX.Util.ConvertitorePx;
-import com.RSA.view.JavaFX.Util.Creatori.CreatoreBottone;
-import com.RSA.view.JavaFX.Util.Creatori.CreatoreHBox;
-import com.RSA.view.JavaFX.Util.Creatori.CreatoreLabel;
-import com.RSA.view.JavaFX.Util.Creatori.CreatoreVBox;
+import com.RSA.view.JavaFX.Util.Creatori.*;
 import javafx.event.EventHandler;
 import javafx.fxml.Initializable;
 import javafx.geometry.Pos;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.ScrollPane;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Font;
+import javafx.scene.text.FontPosture;
 import javafx.scene.text.FontWeight;
 import javafx.scene.text.TextAlignment;
 
@@ -27,6 +27,9 @@ import java.util.ResourceBundle;
 public class VHome implements Initializable {
 
     public VBox homeVBox;
+    public ScrollPane scrollPaneBob;
+    public ScrollPane scrollPaneAlice;
+    public ScrollPane scrollPaneAttuale;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
@@ -43,14 +46,20 @@ public class VHome implements Initializable {
     }
 
     public VBox creaBob() {
-        VBox bobVBox = CreatoreVBox.creaVBox(Pos.CENTER,0.5,0.5,33.3,true);
+        VBox bobVBox = CreatoreVBox.creaVBox(Pos.TOP_CENTER,0.5,0.5,33.3,true);
         Label bobNome = CreatoreLabel.creaLabel("Bob", Font.font("System", FontWeight.BOLD,16),5, TextAlignment.CENTER,0,0,true);
         Button bobKeyButton = CreatoreBottone.creaBottone("Genera Chiave",Pos.CENTER,Font.font("System",FontWeight.BOLD,16),0.5,true);
+        Client bob = new Client("Bob",true);
         bobKeyButton.setOnMouseClicked(new EventHandler<MouseEvent>() {
             @Override
             public void handle(MouseEvent event) {
-                Client bob = new Client("Bob",true);
-                System.out.println(bob.get_privateKey().get_d().toString());
+                scrollPaneAttuale = scrollPaneBob;
+                if (scrollPaneBob == null) {
+                    scrollPaneBob = creaScrollPane(bob);
+                    bobVBox.getChildren().add(scrollPaneBob);
+                } else {
+                    creaScrollPane(bob);
+                }
             }
         });
         bobVBox.getChildren().addAll(bobNome,bobKeyButton);
@@ -59,24 +68,50 @@ public class VHome implements Initializable {
 
 
     public VBox creaEve() {
-        VBox eveVBox = CreatoreVBox.creaVBox(Pos.CENTER,0.5,0.5,33.3,true);
+        VBox eveVBox = CreatoreVBox.creaVBox(Pos.TOP_CENTER,0.5,0.5,33.3,true);
         Label eveNome = CreatoreLabel.creaLabel("Eve", Font.font("System", FontWeight.BOLD,16),5, TextAlignment.CENTER,0,0,true);
         eveVBox.getChildren().addAll(eveNome);
         return eveVBox;
     }
 
     public VBox creaAlice() {
-        VBox aliceVBox = CreatoreVBox.creaVBox(Pos.CENTER,0.5,0.5,33.3,true);
+        VBox aliceVBox = CreatoreVBox.creaVBox(Pos.TOP_CENTER,0.5,0.5,33.3,true);
         Label aliceNome = CreatoreLabel.creaLabel("Alice", Font.font("System", FontWeight.BOLD,16),5, TextAlignment.CENTER,0,0,true);
         Button aliceKeyButton = CreatoreBottone.creaBottone("Genera Chiave",Pos.CENTER,Font.font("System",FontWeight.BOLD,16),0.5,true);
+        Client alice = new Client("Alice", true);
         aliceKeyButton.setOnMouseClicked(new EventHandler<MouseEvent>() {
             @Override
             public void handle(MouseEvent event) {
-                Client alice = new Client("Alice", true);
-                System.out.println(alice.get_privateKey().get_d().toString());
+                scrollPaneAttuale = scrollPaneAlice;
+                if (scrollPaneAlice == null) {
+                    scrollPaneAlice = creaScrollPane(alice);
+                    aliceVBox.getChildren().add(scrollPaneAlice);
+                } else {
+                    creaScrollPane(alice);
+                }
             }
         });
         aliceVBox.getChildren().addAll(aliceNome,aliceKeyButton);
         return aliceVBox;
+    }
+
+    public ScrollPane creaScrollPane(Client client) {
+        VBox content = null;
+        ScrollPane scrollPane = null;
+        if (scrollPaneAttuale != null) {
+            scrollPane = scrollPaneAttuale;
+            content = (VBox) scrollPaneAttuale.getContent();
+            content.getChildren().clear();
+            GeneratoreChiavi.generaChiavi(client,true);
+        } else {
+            content = CreatoreVBox.creaVBox(Pos.TOP_LEFT,0,0,90,true);
+            scrollPane = CreatoreScrollPane.creaScrollPane(content,31,2,2,true);
+        }
+        Label privata = CreatoreLabel.creaLabel("Private Key:",Font.font("System",FontWeight.BOLD,16),5,TextAlignment.LEFT,0.5,0,true);
+        Label p = CreatoreLabel.creaLabel("p: "+client.get_privateKey().get_p().toString(),Font.font("System", FontPosture.REGULAR,12),90,TextAlignment.LEFT,0.5,0,true);
+        Label q = CreatoreLabel.creaLabel("q: "+client.get_privateKey().get_q().toString(),Font.font("System", FontPosture.REGULAR,12),90,TextAlignment.LEFT,0.5,0,true);
+        Label d = CreatoreLabel.creaLabel("d: "+client.get_privateKey().get_d().toString(),Font.font("System", FontPosture.REGULAR,12),90,TextAlignment.LEFT,0.5,0,true);
+        content.getChildren().addAll(privata,p,q,d);
+        return scrollPane;
     }
 }
